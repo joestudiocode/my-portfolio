3cc79da928abf325796fa868ced275a96dc2d674
joestudiocode <joestudiocode@gmail.com>
2025-11-11 09:59:13 +0700

    cleaning code and adjust component

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,8 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import { motion } from "motion/react";
 
-import "../styles/main.scss";
-import Svg from "../assets/Svg";
+import Svg from "./Svg";
 export default function Home() {
   return (
     <>
@@ -24,11 +23,9 @@ export default function Home() {
             <a href="">Project.</a>
           </h1>{" "}
         </div>
-
         <div className="absolute z-10">
-          <Svg/>
+          <Svg />
         </div>
-        {/* <img className="absolute z-20 bottom-26 " src={mySvg} alt="My SVG" /> */}
         <div className=""></div>
         <div className="bg-[#0d0d0d] h-full w-full absolute z-1 bottom-0"></div>
       </div>
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-
+import "./styles/main.scss"
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 // import Navbar from "./nav/Navbar.jsx";
